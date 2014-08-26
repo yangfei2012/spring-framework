3432441a491ddc375a9e75626ed66aa1b04e7d0a
fei <DaturaCheng@gmail.com>
2014-08-26 17:39:10 +0800

    新增测试代码

--- mytest/src/main/java/org/fei/tester/TestBeanFactory.java
+++ mytest/src/main/java/org/fei/tester/TestBeanFactory.java
@@ -1,9 +1,18 @@
 package org.fei.tester;
 
+import org.jruby.RubyProcess;
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.PropertyValue;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConstructorArgumentValues;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
 /**
@@ -13,20 +22,46 @@ import org.springframework.core.io.ClassPathResource;
 public class TestBeanFactory {
 
     public static void main(String args[]) {
-        String path = "test.xml";
 
 
         DefaultListableBeanFactory beanRegistry = new DefaultListableBeanFactory();
 
+        // xml reader bind
         XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanRegistry);
         reader.loadBeanDefinitions("classpath:test.xml");
 
+        // code bind
+        //bindViaCode(beanRegistry);
+
         BeanFactory container = (BeanFactory)beanRegistry;
 
-        //BeanFactory container = new XmlBeanFactory(new ClassPathResource(path));
+
+        //ApplicationContext container = new ClassPathXmlApplicationContext("classpath:test.xml");
 
         TProvider provider = (TProvider)container.getBean("tProvider");
 
         provider.haha();
+    }
+
+    public static BeanDefinitionRegistry bindViaCode(BeanDefinitionRegistry registry) {
+        AbstractBeanDefinition tProvider =
+                new RootBeanDefinition(TProvider.class);
+        AbstractBeanDefinition tlistener =
+                new RootBeanDefinition(ATListener.class);
+
+        ConstructorArgumentValues argValues = new ConstructorArgumentValues();
+        argValues.addIndexedArgumentValue(0, tlistener);
+        tProvider.setConstructorArgumentValues(argValues);
+
+        /*
+        MutablePropertyValues propertyValues = new MutablePropertyValues();
+        propertyValues.addPropertyValue(new PropertyValue("tListener", tlistener));
+        tProvider.setPropertyValues(propertyValues);
+        */
+
+        registry.registerBeanDefinition("tProvider", tProvider);
+        registry.registerBeanDefinition("tListener", tlistener);
+
+        return registry;
     }
 }
